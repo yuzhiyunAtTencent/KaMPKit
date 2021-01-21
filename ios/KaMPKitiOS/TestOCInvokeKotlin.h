74308f6eb53329312ea52f0969492022b45b0b79
yuzhiyun <yuzhiyun@kuaishou.com>
2021-01-21 18:12:28 +0800

    添加oc文件，支持混编

---
+++ ios/KaMPKitiOS/TestOCInvokeKotlin.h
@@ -0,0 +1,17 @@
+//
+//  TestOCInvokeKotlin.h
+//  KaMPKitiOS
+//
+//  Created by yuzhiyun on 2021/1/21.
+//  Copyright © 2021 Touchlab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestOCInvokeKotlin : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/KaMPKitiOS/TestOCInvokeKotlin.m
@@ -0,0 +1,13 @@
+//
+//  TestOCInvokeKotlin.m
+//  KaMPKitiOS
+//
+//  Created by yuzhiyun on 2021/1/21.
+//  Copyright © 2021 Touchlab. All rights reserved.
+//
+
+#import "TestOCInvokeKotlin.h"
+
+@implementation TestOCInvokeKotlin
+
+@end
